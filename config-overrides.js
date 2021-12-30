d3e469c7bd24ea1da13da79c43862004950c5d5d
xiayefeng <xiayu_12@yeah.net>
2021-12-31 01:00:22 +0800

    add errorboundary

--- config-overrides.js
+++ config-overrides.js
@@ -21,7 +21,7 @@ module.exports = override(
   }),
 
   // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
-  process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
+  process.env.BUNDLE_VISUALIZE === 1 && addBundleVisualizer(),
 
   // add an alias for "ag-grid-react" imports
   addWebpackAlias({
